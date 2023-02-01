d6dfd9464b19adf6a6a34e1a4f807f4efb5fc5a2
l00pinfinity <collinskipkemboi001@gmail.com>
2023-02-01 12:01:01 +0300

    style jumbotron margin

--- src/index.ts
+++ src/index.ts
@@ -22,9 +22,47 @@ newsletterForm?.addEventListener("submit", e =>{
   addSubscriber(newSubscriber);
   newsletterEmail.value = "";
 
+//ContactMe
+const contactForm = document.querySelector<HTMLFormElement>("#contactForm");
+const contactName = document.querySelector<HTMLInputElement>("#contactName");
+const contactEmail = document.querySelector<HTMLInputElement>("#contactEmail");
+const contactMessage = document.querySelector<HTMLInputElement>("#contactMessage");
+
+type Message = {
+  id: string,
+  name: string,
+  email: string,
+  message: string,
+  createdAt: Date
+}
+
+contactForm?.addEventListener("submit", e=> {
+  e.preventDefault();
+
+  if (contactEmail?.value == "" || contactEmail?.value == null) return
+  if (contactMessage?.value == "" || contactMessage?.value == null) return
+
+  const newMessage = {
+    id: uuidv4(),
+    name: contactName?.value,
+    email: contactEmail?.value,
+    message: contactMessage.value,
+    createdAt: new Date()
+  }
+
+  addMessage(newMessage);
+})
+
 })
 
 function addSubscriber(newSubscriber: Subscriber) {
   //make api call to add subscriber
-  console.log("Thanks for subscribing " + JSON.stringify(newSubscriber))
+  alert("Thanks for subscribing ");
+  //console.log("Thanks for subscribing " + JSON.stringify(newSubscriber))
 }
+
+function addMessage(newMessage: { id: string; name: string | undefined; email: string; message: string; createdAt: Date; }) {
+  //make api call to add message 
+  //console.log("Message sent successfully " + JSON.stringify(newMessage))
+}
+
